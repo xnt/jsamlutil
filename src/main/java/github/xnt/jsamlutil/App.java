466dff15d90908d36802dd23dc5d7e05f5bdf374
Vicente Plata <xientyto@gmail.com>
2012-03-24 16:51:31 -0700

    First commit

---
+++ src/main/java/github/xnt/jsamlutil/App.java
@@ -0,0 +1,13 @@
+package github.xnt.jsamlutil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
